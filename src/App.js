f719dc2d1d0c9acef828cf934988677ae4ef9ff5
Adelina <adelina.sahlean@gmail.com>
2021-05-19 14:21:16 +0200

    progress

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+const beers = [ { name:'Hoppily Ever After', price: 20}, {name:'Row 26', price: 40}, {name:'GitHop', price: '20kr'}, {name: 'Sleighride', price:'20kr'}, {name:'Mowintime', price: '30'}, {name:'ElHefe', price: '40'}, {name:'FairyTaleAle', price: '20'}, {name:'Mowintime', price: '30'}, {name:'Hollaback Lager', price: '30'}, {name:'Ruined Childhood', price: '30'}, {name: 'Steampunk', price: '30'}];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BeerList beers = {beers}/>
     </div>
   );
+;
 }
+
+function BeerList(props) {
+  // console.log(beers.object);
+  return (
+  <div className="mainS">
+    <section className="beerbox">
+      {props.beers.map(items => <Beer {...items}/>)}
+    </section>
+</div>);
+
+}
+
+function Beer(props) {
+
+  return (
+    <article className="tagbox">
+  <img className="labels" alt="" src= {`${props.name}.png`}></img>
+  <h1>{props.name}</h1>
+  <p>{props.price}</p>
+  <button>Read more</button>
+  <button>+</button>
+      </article>
+  );
+   }
+
 
 export default App;
