723fe682a10799e71ed5e84a1367f8a7ad1c5fb2
clau9156 <clau9156@stud.kea.dk>
2021-05-25 09:21:26 +0200

    tables places behind the numbers

--- src/tables.js
+++ src/tables.js
@@ -5,44 +5,71 @@ export default function Tables() {
           <p className="TablesOrder">Order no. 12345</p>
           <p className="TablesHeader">Select your table:</p>
           <section className="Tables">
-            <div className="TablesGrid">
+            <form className="TablesGrid">
                 <div className="Table">
-                    <img  className="TableImg1 Img1Collum1" alt="Table1" src="./Images/tableS@2x.png"></img>
-                    <div className="TablesText">1</div>
+                    <label>
+                        <input type="radio" name="table" value="table1"></input>
+                        <img  className="TableRow1 Img1Collum1" alt="Table1" src="./Images/tableS@2x.png"></img>
+                        <div className="TablesText">1</div> 
+                    </label> 
                 </div>
                 <div className="Table">
-                    <img  className="TableImg1 Img1Collum2" alt="Table1" src="./Images/tableS@2x.png"></img>
-                    <div className="TablesText">2</div>
+                    <label>
+                        <input type="radio" name="table" value="table2"></input>
+                        <img  className="TableRow1 Img1Collum2" alt="Table1" src="./Images/tableS@2x.png"></img>
+                        <div className="TablesText">2</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg1 Img1Collum3" alt="Table1" src="./Images/tableS@2x.png"></img>
-                    <div className="TablesText">3</div>
+                    <label>
+                        <input type="radio" name="table" value="table3"></input>
+                        <img  className="TableRow1 Img1Collum3" alt="Table1" src="./Images/tableS@2x.png"></img>
+                        <div className="TablesText">3</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg2 Img2Collum1" alt="Table2" src="./Images/tableR@2x.png"></img>
-                    <div className="TablesText">4</div>
+                    <label>
+                        <input type="radio" name="table" value="table4"></input>
+                        <img  className="TableRow2 Img2Collum1" alt="Table2" src="./Images/tableR@2x.png"></img>
+                        <div className="TablesText">4</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg2 Img2Collum2" alt="Table2" src="./Images/tableR@2x.png"></img>
-                    <div className="TablesText">5</div>
+                    <label>
+                        <input type="radio" name="table" value="table5"></input>
+                        <img  className="TableRow2 Img2Collum2" alt="Table2" src="./Images/tableR@2x.png"></img>
+                        <div className="TablesText">5</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg2 Img2Collum3" alt="Table2" src="./Images/tableR@2x.png"></img>
-                    <div className="TablesText">6</div>
+                    <label>
+                        <input type="radio" name="table" value="table6"></input>
+                        <img  className="TableRow2 Img2Collum3" alt="Table2" src="./Images/tableR@2x.png"></img>
+                        <div className="TablesText">6</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg3 Img2Collum1" alt="Table2" src="./Images/tableR@2x.png"></img>
-                    <div className="TablesText">7</div>
+                    <label>
+                        <input type="radio" name="table" value="table7"></input>
+                        <img  className="TableRow3 Img2Collum1" alt="Table2" src="./Images/tableR@2x.png"></img>
+                        <div className="TablesText">7</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg3 Img2Collum2" alt="Table2" src="./Images/tableR@2x.png"></img>
-                    <div className="TablesText">8</div>
+                    <label>
+                        <input type="radio" name="table" value="table8"></input>
+                        <img  className="TableRow3 Img2Collum2" alt="Table2" src="./Images/tableR@2x.png"></img>
+                        <div className="TablesText">8</div>
+                    </label>
                 </div>
                 <div className="Table">
-                    <img  className="TableImg3 Img2Collum3" alt="Table2" src="./Images/tableR@2x.png"></img>
-                    <div className="TablesText">9</div>
+                    <label>
+                        <input type="radio" name="table" value="table9"></input>
+                        <img  className="TableRow3 Img2Collum3" alt="Table2" src="./Images/tableR@2x.png"></img>
+                        <div className="TablesText">9</div>
+                    </label>
                 </div>
-            </div>
+            </form>
           </section>
           {/* <button type="button" className="Button">Proceed with order</button> */}
            {/* <span>
@@ -50,4 +77,5 @@ export default function Tables() {
            </span> */}
       </section>
     );
+    
   }
