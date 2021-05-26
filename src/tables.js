f6474e455d3fbb7c4f121549fb214ce726615a6b
clau9156 <clau9156@stud.kea.dk>
2021-05-26 12:01:59 +0200

    starting form validation

--- src/tables.js
+++ src/tables.js
@@ -1,75 +1,82 @@
 export default function Tables() {
+    // $("input[type='radio'][name='table']:checked").value;
+    const t = document.querySelector('input[name="table"]:checked').value;
+    console.log(t);
+    // if (t == "") {
+    
+    // }
+
     return (
       <section className="Form">
           {/* <OrderNum /> */}
           <p className="TablesOrder">Order no. 12345</p>
           <p className="TablesHeader">Select your table:</p>
           <section className="Tables">
-            <form className="TablesGrid">
+            <div className="TablesGrid">
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table1"></input>
+                        <input type="radio" id="table1" name="table" value="table1"></input>
                         <img  className="TableRow1 Img1Collum1" alt="Table1" src="./Images/tableS@2x.png"></img>
                         <div className="TablesText">1</div> 
                     </label> 
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table2"></input>
+                        <input type="radio" id="table2" name="table" value="table2"></input>
                         <img  className="TableRow1 Img1Collum2" alt="Table1" src="./Images/tableS@2x.png"></img>
                         <div className="TablesText">2</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table3"></input>
+                        <input type="radio" id="table3" name="table" value="table3"></input>
                         <img  className="TableRow1 Img1Collum3" alt="Table1" src="./Images/tableS@2x.png"></img>
                         <div className="TablesText">3</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table4"></input>
+                        <input type="radio" id="table4" name="table" value="table4"></input>
                         <img  className="TableRow2 Img2Collum1" alt="Table2" src="./Images/tableR@2x.png"></img>
                         <div className="TablesText">4</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table5"></input>
+                        <input type="radio" id="table5" name="table" value="table5"></input>
                         <img  className="TableRow2 Img2Collum2" alt="Table2" src="./Images/tableR@2x.png"></img>
                         <div className="TablesText">5</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table6"></input>
+                        <input type="radio" id="tabl6"  name="table" value="table6"></input>
                         <img  className="TableRow2 Img2Collum3" alt="Table2" src="./Images/tableR@2x.png"></img>
                         <div className="TablesText">6</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table7"></input>
+                        <input type="radio" id="table7" name="table" value="table7"></input>
                         <img  className="TableRow3 Img2Collum1" alt="Table2" src="./Images/tableR@2x.png"></img>
                         <div className="TablesText">7</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table8"></input>
+                        <input type="radio" id="tabl8" name="table" value="table8"></input>
                         <img  className="TableRow3 Img2Collum2" alt="Table2" src="./Images/tableR@2x.png"></img>
                         <div className="TablesText">8</div>
                     </label>
                 </div>
                 <div className="Table">
                     <label>
-                        <input type="radio" name="table" value="table9"></input>
+                        <input type="radio" id="table9" name="table" value="table9"></input>
                         <img  className="TableRow3 Img2Collum3" alt="Table2" src="./Images/tableR@2x.png"></img>
                         <div className="TablesText">9</div>
                     </label>
                 </div>
-            </form>
+            </div>
           </section>
           {/* <button type="button" className="Button">Proceed with order</button> */}
            {/* <span>
